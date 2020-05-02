1207eb7417b5d3cfad3baf06e39ba5187609c7f3
Luc Freyermuth <lucfreyermuth@gmail.com>
2020-05-02 23:14:12 +0200

    header

---
+++ src/routes/_layout.svelte
@@ -0,0 +1,26 @@
+<style>
+  main {
+    flex: 1;
+  }
+
+  @media screen and (max-width: 768px) {
+    img {
+      max-width: 50%;
+    }
+  }
+
+  @media screen and (min-width: 769px) {
+    img {
+      max-width: 15%;
+    }
+  }
+</style>
+
+<div class="header is-center">
+  <a href="/">
+    <img src="images/logo.webp" alt="shadow hunters logo" />
+  </a>
+</div>
+<main>
+  <slot />
+</main>
