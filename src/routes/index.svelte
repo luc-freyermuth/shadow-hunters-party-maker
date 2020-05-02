498daf0ccfa912dd64cc1f593e943d836bdab410
Luc Freyermuth <lucfreyermuth@gmail.com>
2020-05-02 16:19:56 +0200

    prettier config, page creation, global css

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,18 @@
-<button class="button is-primary">Créer une partie</button>
-<a href="card-list"><button class="button is-primary">Liste des cartes</button></a>
+<style>
+  a:not(:last-child) {
+    margin-bottom: 1em;
+  }
+</style>
+
+<div class="container is-fluid is-vertical-center">
+  <a href="create-game" class="is-fullwidth">
+    <button class="button is-primary is-fullwidth is-large">
+      Créer une partie
+    </button>
+  </a>
+  <a href="card-list" class="is-fullwidth">
+    <button class="button is-primary is-fullwidth is-large">
+      Liste des cartes
+    </button>
+  </a>
+</div>
---
+++ src/routes/play/[slug].svelte
@@ -0,0 +1,15 @@
+<script context="module">
+    export async function preload(page, session) {
+		// the `slug` parameter is available because this file
+		// is called [slug].svelte
+		const { slug } = page.params;
+
+		return { slug };
+	}
+</script>
+
+<script>
+    export let slug;
+</script>
+
+{slug}
