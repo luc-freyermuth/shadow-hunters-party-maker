fc265f9e8ce036452e16c62879106883b71b4d2e
Luc Freyermuth <lucfreyermuth@gmail.com>
2020-05-06 00:57:59 +0200

    add search, styling

--- src/routes/card-list.svelte
+++ src/routes/card-list.svelte
@@ -7,14 +7,38 @@
     cardsStore.subscribe(newCards => {
         cards = [...newCards];
     });
+
+    let filteredCards;
+    let search = '';
+
+    $: filteredCards = cards.filter(c => c.name.toLowerCase().trim().includes(search.toLowerCase().trim()));
 </script>
 
-<h1 class="title is-1 is-center">Liste des cartes</h1>
+<style>
+    .cards {
+        margin-top: 20px;
+    }
+</style>
 
-<div class="columns">
-    {#each cards as card}
-        <div class="column is-3-fullhd is-4-desktop is-6-tablet is-12-mobile is-inline-block">
-            <Card {card} />
+<div class="container is-fluid">
+    <h2 class="title is-2 is-center">Liste des cartes</h2>
+
+    <div class="field">
+        <p class="control has-icons-left has-icons-right">
+            <input class="input is-primary" type="text" placeholder="Rechercher..." bind:value={search} />
+            <span class="icon is-small is-left">
+                <i class="gg-search"></i>
+            </span>
+        </p>
+    </div>
+
+    <div class="cards">
+        <div class="columns">
+            {#each filteredCards as card}
+                <div class="column is-3-fullhd is-4-desktop is-6-tablet is-12-mobile is-inline-block">
+                    <Card {card} />
+                </div>
+            {/each}
         </div>
-    {/each}
+    </div>
 </div>
