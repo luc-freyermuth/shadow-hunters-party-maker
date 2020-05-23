c29aa867eedd0088546f944dc303674d4e859adc
Luc Freyermuth <lucfreyermuth@gmail.com>
2020-05-23 16:18:31 +0200

    first try for single choice

--- src/peer2peer/peer-host.js
+++ src/peer2peer/peer-host.js
@@ -91,7 +91,76 @@ class PeerHost {
   }
 
   startGame(gameConfig) {
-    console.log(gameConfig);
+    const teams = this.createTeams(gameConfig.shadowHuntersCount);
+
+    let cards = gameConfig.cards;
+    if (gameConfig.excludeAllPreviouslyPlayedCards) {
+      const previouslyPlayedCardNames = this.players.map(p => p.previousCard).filter(card => !!card).map(card => card.name);
+      cards = cards.filter(card => !previouslyPlayedCardNames.includes(card.name));
+    }
+
+    switch (gameConfig.options.mode) {
+      case 'single':
+        this.setPlayersRandomCardForSingleMode(
+          cards, 
+          teams,
+          gameConfig.options.onlyOneWithSameLetter,
+          gameConfig.options.modeOptions.preventSame,
+          gameConfig.options.modeOptions.preventSameLetter
+        );
+        break;
+    }
+  }
+
+  createTeams(shadowHuntersCount) {
+    const shuffledPlayers = this.shuffleArray(this.players);
+    return {
+      hunter: shuffledPlayers.slice(0, shadowHuntersCount),
+      shadow: shuffledPlayers.slice(shadowHuntersCount, shadowHuntersCount * 2),
+      neutral: shuffledPlayers.slice(shadowHuntersCount * 2, shuffledPlayers.length)
+    }
+  }
+
+  setPlayersRandomCardForSingleMode(cards, teams, onlyOneWithSameLetter, preventSame, preventSameLetter) {
+    for (const [teamName, teamMembers] of Object.entries(teams)) {
+      let foundSolution = false;
+      while (!foundSolution) {
+        let remainingCards = cards.filter(card => card.team === teamName);
+        try {
+          for (const teamMember of teamMembers) {
+            let teamMemberChoices = [...remainingCards];
+
+            if (preventSame && teamMember.previousCard) {
+              teamMemberChoices = teamMemberChoices.filter(card => card.name !== teamMember.previousCard.name);
+            }
+            if (preventSameLetter && teamMember.previousCard) {
+              console.log('excluding previous card with same letter');
+              teamMemberChoices = teamMemberChoices.filter(card => !card.name.startsWith(teamMember.previousCard.name.slice(0,1)));
+            }
+
+            teamMember.currentCard = this.shuffleArray(remainingCards).shift();
+
+            remainingCards = remainingCards.filter(card => card.name !== teamMember.currentCard.name);
+
+            if (onlyOneWithSameLetter) {
+              remainingCards = remainingCards.filter(card => !card.name.startsWith(teamMember.currentCard.name.slice(0,1)));
+            }
+          }
+          foundSolution = true;
+        } catch (error) {
+          console.error(error);
+          foundSolution = false;
+        }
+      }
+    }
+
+    this.players.forEach(player => {
+      player.previousCard = {
+        ...player.currentCard
+      }
+    });
+
+    console.log(this.players);
   }
 
   // Actions //
@@ -100,14 +169,16 @@ class PeerHost {
     if (this.getPlayerByName(name)) {
       const existingPlayer = this.getPlayerByName(name);
       existingPlayer.peerId = connection.peer;
-      if (existingPlayer.cardData) {
-        this.sendCardData(existingPlayer);
+      if (existingPlayer.currentCard) {
+        this.sendCurrentCard(existingPlayer);
       }
     } else {
       this.players.push({
         name,
-        cardData: null,
-        peerId: connection.peer
+        peerId: connection.peer,
+        currentCard: null,
+        previousCard: null,
+        previousChoices: []
       });
     }
     this.playersChanged();
@@ -118,13 +189,13 @@ class PeerHost {
 
   // Send //
 
-  sendCardData(player) {
-    if (player.cardData) {
+  sendCurrentCard(player) {
+    if (player.currentCard) {
       const connection = this.getConnectionByPlayer(player);
       if (connection) {
         connection.send({
-          type: 'cardData',
-          data: player.cardData
+          type: 'currentCard',
+          data: player.currentCard
         });
       }
     }
@@ -148,6 +219,17 @@ class PeerHost {
         isConnected: player.peerId !== null
       }
     }));
+  }
+
+  shuffleArray(array) {
+    const a = [...array];
+    for (let k = 0; k < 1000; k++) {
+      for (let i = a.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [a[i], a[j]] = [a[j], a[i]];
+      }
+    }
+    return a;
   }
 }
 
