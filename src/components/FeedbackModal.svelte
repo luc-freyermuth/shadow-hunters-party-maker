88767cd4789c75471e70716b77e2da2edeb4ae6e
Luc Freyermuth <lucfreyermuth@gmail.com>
2020-06-20 15:51:41 +0200

    match star size to width

--- src/components/FeedbackModal.svelte
+++ src/components/FeedbackModal.svelte
@@ -10,7 +10,7 @@
   const dispatch = createEventDispatcher();
 
   function pickedFeedback() {
-    dispatch('pickedFeedback', { win, funLevel })
+    dispatch('pickedFeedback', { win, funLevel });
   }
 </script>
 
@@ -29,11 +29,24 @@
 
   label.star {
     float: right;
-    padding: 10px;
-    font-size: 3rem;
+    margin: 5px;
+    padding: 5px;
     color: #444;
     transition: all 0.2s;
     font-family: 'FontAwesome';
+    line-height: 1;
+  }
+
+  @media screen and (max-width: 768px) {
+    label.star {
+      font-size: calc((100vw - 200px) / 5);
+    }
+  }
+
+  @media screen and (min-width: 769px) {
+    label.star {
+      font-size: 3rem;
+    }
   }
 
   input.star:checked ~ label.star:before {
@@ -99,57 +112,27 @@
       <h2 class="title is-2">La partie t'a plu ?</h2> -->
 
       <div class="stars">
-        <input
-          class="star star-5"
-          id="star-5"
-          type="radio"
-          name="star"
-          bind:group={funLevel}
-          value={5}
-        />
-        <label class="star star-5" for="star-5" />
-        <input
-          class="star star-4"
-          id="star-4"
-          type="radio"
-          name="star"
-          bind:group={funLevel}
-          value={4}
-        />
-        <label class="star star-4" for="star-4" />
-        <input
-          class="star star-3"
-          id="star-3"
-          type="radio"
-          name="star"
-          bind:group={funLevel}
-          value={3}
-        />
-        <label class="star star-3" for="star-3" />
-        <input
-          class="star star-2"
-          id="star-2"
-          type="radio"
-          name="star"
-          bind:group={funLevel}
-          value={2}
-        />
-        <label class="star star-2" for="star-2" />
-        <input
-          class="star star-1"
-          id="star-1"
-          type="radio"
-          name="star"
-          bind:group={funLevel}
-          value={1}
-        />
-        <label class="star star-1" for="star-1" />
+        {#each [5, 4, 3, 2, 1] as value}
+          <input
+            class="star star-{value}"
+            id="star-{value}"
+            type="radio"
+            name="star"
+            bind:group={funLevel}
+            {value}
+          />
+          <label class="star star-{value}" for="star-{value}" />
+        {/each}
       </div>
 
       <p />
     </section>
     <footer class="modal-card-foot">
-      <button class="button is-primary is-fullwidth" disabled={!formIsValid} on:click={pickedFeedback}>
+      <button
+        class="button is-primary is-fullwidth"
+        disabled={!formIsValid}
+        on:click={pickedFeedback}
+      >
         Valider
       </button>
     </footer>
