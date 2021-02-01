33a61c8105b4b085cc5d083c33637ab5f12558bd
Luc Freyermuth <lucfreyermuth@gmail.com>
2021-02-01 18:28:29 +0100

    reset music when clicking buton again

--- src/components/ThemePlayer.svelte
+++ src/components/ThemePlayer.svelte
@@ -3,9 +3,17 @@
 
   export let card: Character;
   let paused: boolean = true;
+  let currentTime: number = 0;
 
   $: src = '/cards/themes/' + card?.theme;
   $: disabled = !card?.theme;
+
+  $: {
+    if (paused) {
+      console.log('reset time');
+      currentTime = 0;
+    }
+  }
 </script>
 
 <style>
@@ -20,7 +28,7 @@
 
 <button class="button is-primary is-fullwidth mt-4" on:click={() => (paused = !paused)} {disabled}>
   Jouer le thème
-  <i class:gg-play-button-o={paused} class:gg-play-pause-o={!paused} />
+  <i class:gg-play-button-o={paused} class:gg-play-stop-o={!paused} />
 </button>
 
-<audio {src} controls bind:paused />
+<audio {src} controls bind:paused bind:currentTime><track kind="captions" /></audio>
